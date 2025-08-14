01b223e635b0be6538406eb5a443bf199a98aa48
Amartheone <d.amarnathpatro20@gmail.com>
2025-08-14 21:24:08 +0530

    editor page updated with loader

--- app/(main)/editor/[projectid]/page.jsx
+++ app/(main)/editor/[projectid]/page.jsx
@@ -2,10 +2,11 @@
 
 import { CanvasContext } from "@/context/context";
 import { useConvexQuery } from "@/hooks/use-convex-query";
-import { Monitor } from "lucide-react";
+import { Loader2, Monitor } from "lucide-react";
 import { useParams } from "next/navigation";
 import { api } from "@/convex/_generated/api";
 import React, { useState } from "react";
+import { RingLoader } from "react-spinners";
 
 const Editor = () => {
   const params = useParams();
@@ -19,8 +20,33 @@ const Editor = () => {
   const {
     data: project,
     isLoading,
-    error
-  } = useConvexQuery(api.projects.getProject, {projectId});
+    error,
+  } = useConvexQuery(api.projects.getProject, { projectId });
+
+  if (isLoading) {
+    return (
+      <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+        <div className="flex flex-col items-center gap-4">
+          <Loader2 className="h-8 w-8 animate-spin text-cyan-400" />
+          <p className="text-white/70">Loading...</p>
+        </div>
+      </div>
+    );
+  }
+
+  if (error || !project) {
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-2xl ">
+          Project Not Found
+          <p>
+            The project you're looking for doesn't exist or you dont have access
+            to it.
+          </p>
+        </h1>
+      </div>
+    </div>;
+  }
 
   return (
     <CanvasContext.Provider
@@ -51,7 +77,24 @@ const Editor = () => {
       </div>
 
       <div className="hidden lg:block min-h-screen bg-slate-900">
-        Editor: {projectId}
+        <div>
+          {processingMessage && (
+            <div className="fixed inset-0 bg-black/50 backdrop-blur-xs z-50 flex items-center justify-center">
+              <div className="rounded-lg p-6 flex flex-col items-center gap-4">
+                <RingLoader color="#fff"/>
+                <div className="text-center">
+                  <p className="text-white font-medium">{processingMessage}</p>
+                  <p className="text-white/70 text-sm mt-1">Please wait, do not switch tabs or navigate away</p>
+                </div>
+              </div>
+            </div>
+          )}
+        {/* Top Bar */}
+        <div className="flex flex-1 overflow-hidden"> 
+          {/* sidebar */}
+          <div className="flex bg-slate-800">{/* Canvas */}</div>
+        </div>
+      </div>
         </div>
     </CanvasContext.Provider>
   );
