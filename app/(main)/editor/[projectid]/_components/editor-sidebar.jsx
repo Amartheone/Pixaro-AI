99f0f120a16d238ea6b79d78251e81f54a9433b2
Amartheone <d.amarnathpatro20@gmail.com>
2025-08-24 22:59:56 +0530

    background tool

--- app/(main)/editor/[projectid]/_components/editor-sidebar.jsx
+++ app/(main)/editor/[projectid]/_components/editor-sidebar.jsx
@@ -12,6 +12,7 @@ import CropContent from "./tools/crop";
 import React from "react";
 import ResizeControls from "./tools/resize";
 import AdjustControls from "./tools/adjust";
+import BackgroundControls from "./tools/ai-background";
 
 const TOOL_CONFIGS = {
   resize: {
@@ -87,6 +88,8 @@ function renderToolConfig(activeTool, project) {
       return <ResizeControls project={project} />;
     case "adjust":
       return <AdjustControls project={project} />;
+    case "background":
+      return <BackgroundControls project={project} />;
 
     default:
       return <div className="text-white">Select a tool to get started</div>;
---
+++ app/(main)/editor/[projectid]/_components/tools/ai-background.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const UNSPLASH_ACCESS_KEY = process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY;
+const UNSPLASH_API_URL = "https://api.unsplash.com"
+
+const BackgroundControls = ({ project }) => {
+  return <div>BackgroundControls</div>;
+};
+
+export default BackgroundControls;
